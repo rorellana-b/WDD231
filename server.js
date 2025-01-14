c4c5ae779a14b2845ebf6cd4afa04d3106d2920b
rorellana-b <ronalbalmoreorellanabonilla@192.168.1.4>
2025-01-13 20:46:53 -0600

    adding files and style

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
-app.set("layout", "./layouts/layout") //not at views root
+app.set("layout", "layouts/layout") //not at views root
 
 /* ***********************
  * Local Server Information
